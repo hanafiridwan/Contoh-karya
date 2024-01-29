d55b81ead50d7d6aa896397e0738ab4419861a4d
ridwanhanafi <ridwanacmad619@gmail.com>
2024-01-29 12:50:03 +0700

    responsive analystyc web

---
+++ responsive web/orders.js
@@ -0,0 +1,22 @@
+const Orders = [
+    {
+        productName: 'Javascript tutorial',
+        productNumber: '98989',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+
+    {
+        productName: 'CSS Full Course',
+        productNumber: '87878',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+
+    {
+        productName: 'Flex-box tutorial',
+        productNumber: '76767',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    }
+]
